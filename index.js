74e78b0ae549f7dc726f0fce8bcb18393f4a6a58
Genevieve Schultz <genschultz@gmail.com>
2018-06-13 22:24:57 -0700

    finished deliverable

--- index.js
+++ index.js
@@ -22,18 +22,53 @@ app.get('/players', function(req, res) {
 app.post('/players', function (req, res) {
   var players = fs.readFileSync('./data.json');
   players = JSON.parse(players);
-  players.push({name: req.body.name, position: req.body.color});
+  players.push({name: req.body.name, position: req.body.position});
   fs.writeFileSync('./data.json', JSON.stringify(players));
   res.json(players);
   console.log(req.body);  
 });
 
-// TODO:
 //GET /players/:id - gets one player
+app.get('/players/:id', function(req, res) {
+  var players = fs.readFileSync('./data.json');
+  players = JSON.parse(players);
+  var playerIndex = req.params.id;
+  if (playerIndex >= players.length) {
+    console.log('that is not a player');
+  } else {
+    res.json(players[playerIndex]);
+    console.log({ player: players[playerIndex] });
+  }
+});
 
 //PUT /players/:id - updates one player
+app.put('/players/:id', function(req, res) {
+  var players = fs.readFileSync('./data.json');
+  players = JSON.parse(players);
+  var playerIndex = req.params.id;
+  if (playerIndex >= players.length) {
+    console.log('that is not a player');
+  } else {
+    players.splice(playerIndex, 1, { name: req.body.name, position: req.body.position });
+    fs.writeFileSync('./data.json', JSON.stringify(players));
+    res.send(players);
+    console.log(req.body);
+  }
+});
 
 //DELETE /players/:id - deletes one player
-
+app.delete('/players/:id', function(req, res) {
+  var players = fs.readFileSync('./data.json');
+  players = JSON.parse(players);
+  var playerIndex = req.params.id;
+  if (playerIndex >= players.length) {
+    console.log('that is not a player');
+  } else {
+    players.splice(playerIndex, 1);
+    fs.writeFileSync('./data.json', JSON.stringify(players));
+    res.send(players);
+    console.log(req.body);
+  }
+});
 
 app.listen(3000);
